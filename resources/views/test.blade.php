a708756dc0227a8fe7d027e6d0aaacf259aab871
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2021-12-25 11:39:50 +0100

    initial commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>testing</title>
+<link rel="stylesheet" href="<?php echo asset('css/style.css')?>" type="text/css"> 
+</head>
+<body>
+ test css
+
+</body>
+</html>
